4417b7018d61987293c1d4604654dfb4223489de
L. Kärkkäinen <Tronic@users.noreply.github.com>
2023-10-10 00:14:36 +0100

    Remove deprecated pgk_resources module. Using hardcoded version number instead.

--- tracerite/__init__.py
+++ tracerite/__init__.py
@@ -1,10 +1,6 @@
-import pkg_resources
-
 from .html import html_traceback
 from .notebook import load_ipython_extension, unload_ipython_extension
 from .trace import extract_chain
 
 __all__ = ["html_traceback", "extract_chain"]
-__version__ = pkg_resources.require(__name__)[0].version
-
-del pkg_resources
+__version__ = "1.1.1"
