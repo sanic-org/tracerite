beb78fca919066675694a912a634509e3184af28
Adam Hopkins <adam@amhopkins.com>
2025-06-17 21:00:15 -0400

    Update __init__.py

--- tracerite/__init__.py
+++ tracerite/__init__.py
@@ -3,4 +3,4 @@ from .notebook import load_ipython_extension, unload_ipython_extension
 from .trace import extract_chain
 
 __all__ = ["html_traceback", "extract_chain"]
-__version__ = "1.1.1"
+__version__ = "1.1.2"
