f780b5610b3aa00f3ec9d8ae44eecf6105e3ff80
L. Karkkainen <tronic@users.noreply.github.com>
2026-07-12 20:49:41 +0000

    Add granular load/unload hooks and notebook suppression support

--- tracerite/__init__.py
+++ tracerite/__init__.py
@@ -1,5 +1,14 @@
 from .fastapi import patch_fastapi
-from .hooks import load, unload
+from .hooks import (
+    load,
+    load_hooks,
+    load_logging_capture,
+    load_suppressions,
+    unload,
+    unload_hooks,
+    unload_logging_capture,
+    unload_suppressions,
+)
 from .html import html_traceback
 from .inspector import extract_variables, prettyvalue
 from .notebook import load_ipython_extension, unload_ipython_extension
@@ -8,7 +17,13 @@ from .tty import tty_traceback
 
 __all__ = [
     "load",
+    "load_hooks",
+    "load_logging_capture",
+    "load_suppressions",
     "unload",
+    "unload_hooks",
+    "unload_logging_capture",
+    "unload_suppressions",
     "tty_traceback",
     "html_traceback",
     "extract_chain",
